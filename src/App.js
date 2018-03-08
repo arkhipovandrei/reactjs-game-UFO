52e369653690ddeb98525c75fc323f7d99f1be44
andrei <arkhipovandrei.p@gmailcom>
2018-03-08 17:58:03 +0300

    add sky and ground

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import Canvas from './components/Canvas';
 
 class App extends Component {
-
   render() {
-    const {message} = this.props;
-
     return (
-      <div className="App">
-        <h1>{message}</h1>
-      </div>
+      <Canvas />
     );
   }
 }
-
-App.propTypes = {
-  message: PropTypes.string.isRequired,
-};
 
 export default App;
